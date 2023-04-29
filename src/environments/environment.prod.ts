acfe6d14d7484942d01ef5bc281d944830f85068
jym126 <jym126@hotmail.com>
2023-04-29 22:15:13 +0200

    uri

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   url: '../assets/contactos/',
-  uriList: 'mongodb+srv://jym126:Sherimarlen1*@e-pa.001xrlh.mongodb.net/listas'
+  uriList: 'https://epa.onrender.com'
 };
